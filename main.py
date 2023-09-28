938741ced8e40edc63c8ec752d50a9053c7f3316
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-09-27 17:41:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,108 @@
-import os, pick
+import os, pick, shutil, urllib.request, time
+from pathlib import Path
 
-def intense_warning():
-    os.system("cls && color 47")
-    input("IF YOU HAVE BOUGHT THIS TOOL SEPERATELY OR AS PART OF A BUNDLE, YOU HAVE BEEN SCAMMED!\n\nTHIS TOOL IS FREE SOFTWARE, PLEASE ASK THE SELLER FOR YOUR MONEY BACK\nAND DOWNLOAD FROM AN OFFICIAL SEVENWORKS.EU.ORG SERVICE BECAUSE THE SELLER\nCOULD HAVE IMPLEMENTED A MALICIOUS SCRIPT WHICH COULD HARM YOUR DEVICE.\n\nTHANK YOU!\n\nPress enter to continue...")
-    os.system("cls && color 07")
+def clear(): os.system('cls' if os.name == 'nt' else 'clear')
 
-intense_warning()
+banner = """
+---------=----------------------------=-----------------
+---------=--------------=-::----------=-----------------
+---------=------------=-..=:.:--------=-----------------
+---------=----------=-..=#%#+..:------=-----------------
+---------=--------=-..=#%#####+..:=---=-----------------
+---------=------=-..=#%##%######+..:=-=-----------------          .==                         .==
+---------==---=-..=#%%#%%*#%######=..-==----------------        ..=@@.    ::.    ..:.       ..-@@.
+---------====-:.=#%%#%%*:::=#%######=..-----------------     .*@@#%@@. :##**%#: :#**#@#: .*@@##@@.
+---------==-..=#%%%%%*:-*%#=:=########=..-=-------------     #@#  -@@. @@#++%@# .-++*@@+ *@%  :@@.
+==========..=#%%%%%%%*:-*%#=:=#%%++#####=..-=-----------     #@%. =@@. @@#--:-- %@#::@@+ *@%. =@@.
+------=-..-#%%#%%**%%%%*--:=#%%*:.::+#####=..-==-====-=-     .*@%#*%%. .*%####+ +%%*+#%= .*%%#*%%.
+------..-#%%#%%*-::-#%%%%*#%%*:.=##+ :*#####=..--------:                   ..
+----:.-#%###%#::*%%+:-#%#%%*-.=###+:-*#######*=.:----:::     :**.   **=                   +%*
+---: =%%####%#--*%#+:=#%%*-.=#%%+::*#######***#+ .::::::     =@@:   @@*  =++++-.   :=**+- *@%  -=-
+:::-: :+#####%%*-::=#%%*-.=#%%+::*%####******+: .:::::::     -@@%%%%@@*  ===+%@%  #@%+=+* *@#:#@*.
+::::::. :+######%**%%*-.=#%%+::+##*********+: .:::::::::     -@@=:::@@* .+#*=*@@ :@@:     *@@%@%
+::::::::. :+#######+-.-*##+::+##*********+: .:::::::::::     =@@:   @@* =@@=-*@@  #@%+=+* *@% +@@-
+:::::::::-. :+###*#= :*#+::+##*********+: .:::::::::::::     .==.   -=:  -++=-==   :=+*+- :=-  :==
+:::::::::---. :+##*#*-..:+##*********+: .:::::::::::::::
+:::::::::-::::. :+#**#*+##*********+: .:::::::::::::::::
+:::::::::-::::::. :+*************+: .:-:::::::::::::::::
+:::::::::-::::::::. :+*********+- .:::--::::::::::::::::
+:::::::::--:::::::-:. :+*****+- .:-:::-:::::::::::::::::
+:::::::::-::::::::::::. :+*+- .:::::::::::::::::::::::::
+:::::::::-::::::::::::::. :..:::::::::::::::::::::::::::
+:::::::::-::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::-::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::-::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::-::::::::::::::::::::::::::::::::::::::::::::::
+DEADHACK AND ITS CONTENTS ARE FREE SOFTWARE, IF YOU HAVE PURCHASED THIS SEPERATELY OR BUNDLED THEN YOU HAVE BEEN SCAMMED! PLEASE DELETE THIS AND DOWNLOAD A LEGIT COPY OF DEADHACK THROUGH ANY SOURCE ON SEVENWORKS.EU.ORG, THANKS!
+"""
+print(banner)
+time.sleep(2.5)
+clear()
 
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+user_home = str(Path.home())
+output_directory = os.path.join(user_home, ".sevenworks") if os.name == 'posix' else os.path.join(user_home, ".sevenworks")
 
-def tool1():
-    input("hello there")
+version = "0.1"
 
-def tool2():
-    input("hello there again")
+file_urls = [
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/Proxy_Server.exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/Tunnel_(For_GDProxy).exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/Song_Server_Location_Finder.exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/Level_Report_Spam.exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/Find_All_Users.exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/Comment_Downloader.exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/Message_Spammer_(Find_User_in_Recent_Tab_Method).exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/Message_Spammer.exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/GDPS_Account_Creator.exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/Comment_Filter_Bypass.exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/Comment_Spam_(Single_Account).exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/GDPS_Likebot.exe",
+    "https://github.com/SevenworksDev/DeadHack/raw/main/robtop-hate-gang/GD_Remote_PC.exe"
+]
 
-def tool3():
-    input("yuh huh!")
+time.sleep(4)
+clear()
+
+def download_files(file_urls, output_directory):
+    for url in file_urls:
+        filename = url.split('/')[-1]
+        filepath = os.path.join(output_directory, filename)
+        urllib.request.urlretrieve(url, filepath)
+        print(f"Downloaded: {filename}")
+
+if not os.path.exists(output_directory):
+    print(f'Welcome to deadHack {version}, One of the most badass hack menus in Geometry Dash!\n\nSince your [.sevenworks] folder is missing in your user folder, This will now install all tools as compiled executables so you dont have to install Python 3.8 or anything extra.\n\n\nPress enter to continue...')
+    input()
+    os.makedirs(output_directory)
+    download_files(file_urls, output_directory)
 
 def display_menu():
+    files = os.listdir(output_directory)
     options = [
-        "Tool 1",
-        "Tool 2",
-        "Tool 3"
+        filename.replace('_', ' ').replace('.exe', '') for filename in files
     ]
 
     title = "Select a tool:"
     picker = pick.Picker(options, title)
     tool = picker.start()
-    
+
     return tool[0] if tool else None
 
 if __name__ == "__main__":
     os.system('color 17')
-    
+
     try:
         while True:
             clear()
             choice = display_menu()
-            
+
             if choice is None:
                 break
 
-            if choice == "Tool 1":
-                tool1()
-            elif choice == "Tool 2":
-                tool2()
-            elif choice == "Tool 3":
-                tool3()
-            
+            selected_filename = choice.replace(' ', '_') + '.exe'
+            selected_filepath = os.path.join(output_directory, selected_filename)
+            os.system(selected_filepath)
+
         os.system('color 07')
 
     except KeyboardInterrupt:
